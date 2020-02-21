585d596d4e67392286075ec9c8f5b2401f08888b
Souza <debora.souza@wunderman.com>
2020-02-21 16:33:29 -0300

    Altera font-size do botão de login, altera o readme

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -55,6 +55,7 @@ export const LoginBtn = styled.button`
   display: inline-block;
   width: 100%;
   height: 50px;
+  font-size: 16px;
   margin-top: 20px;
   border: none;
   background: linear-gradient(120deg, #3498db, #8e44ad, #3498db);
